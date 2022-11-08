4cc114c400a8a67726b63139d1548724fad48264
Clement Fung <clement.y.fung@gmail.com>
2022-11-08 10:34:51 -0500

    Add airflow plot script

--- main_analysis.py
+++ main_analysis.py
@@ -27,6 +27,48 @@ def plot_annual(column, title='Sample Title', filename='temp.pdf'):
     plt.savefig(filename)
     plt.close()
 
+def airflowplots():
+
+    df_vav = load_csv('ZoneAirTerminalVAVDamperPosition.csv')
+    df_flow = load_csv('ZoneMechanicalVentilationMassFlowRate.csv')
+
+    'ZoneMechanicalVentilationMassFlowRate.csv'
+    'ZoneAirTerminalVAVDamperPosition.csv'
+    'ZoneMeanAirTemperature.csv'
+
+    df = load_csv('building_data.csv')
+    open_idx = np.where(df['Operating Time'] == 'Yes')[0]
+    
+    fig, ax = plt.subplots(3, 2, figsize=(12, 16))
+
+    for i in range(6):
+        
+        starter = 20 # 14 or 20
+        col_idx = starter + i
+        
+        x = i // 2
+        y = i % 2
+
+        print(f'Collecting {df_vav.columns[col_idx]} and {df_flow.columns[col_idx]}')
+        
+        location = df_vav.columns[col_idx][:14]
+        ax[x, y].set_title(f'{location}', fontsize=14)
+        ax[x, y].scatter(df_vav.iloc[open_idx,col_idx].values, df_flow.iloc[open_idx,col_idx].values)
+        #ax[x, y].set_xlabel('Zone VAV Damper', fontsize=14)
+        #ax[x, y].set_ylabel('Zone Airflow', fontsize=14)
+    
+    ax[0, 0].set_ylabel('Zone Airflow', fontsize=14)
+    ax[1, 0].set_ylabel('Zone Airflow', fontsize=14)
+    ax[2, 0].set_ylabel('Zone Airflow', fontsize=14)
+
+    ax[2, 0].set_xlabel('Zone VAV Damper', fontsize=14)
+    ax[2, 1].set_xlabel('Zone VAV Damper', fontsize=14)
+
+    fig.tight_layout()
+    plt.savefig(f'corridor-flow.pdf')
+    plt.close()
+
+
 def main():
 
     # Plot some exploratory information
@@ -34,30 +76,31 @@ def main():
     print(f'Plotting {df.columns[6]}')
     plot_annual(df.iloc[:,6].values, title='Occupants', filename='occupants.pdf')
 
-    df = load_csv('ZoneThermostatCoolingSetpointTemperature.csv')
-    print(f'Plotting {df.columns[18]}')
-    plot_annual(df.iloc[:,18].values, title='Cooling Setpoint', filename='cooling-setpoint.pdf')
-    
-    df = load_csv('ZoneThermostatHeatingSetpointTemperature.csv')
-    print(f'Plotting {df.columns[18]}')
-    plot_annual(df.iloc[:,18].values, title='Heating Setpoint', filename='heating-setpoint.pdf')
-    
     df = load_csv('ZoneAirTerminalVAVDamperPosition.csv')
-    print(f'Plotting {df.columns[18]}')
-    plot_annual(df.iloc[:,18].values, title='VAV Damper', filename='vav-damper.pdf')
+    print(f'Collecting {df.columns[18]}')
+    df_heatset = df.copy()
 
-    df = load_csv('ZoneMeanAirTemperature.csv')
-    print(f'Plotting {df.columns[18]}')
-    plot_annual(df.iloc[:,18].values, title='Air Temperature', filename='air-temp.pdf')
+    'ZoneMechanicalVentilationMassFlowRate.csv'
+    'ZoneAirTerminalVAVDamperPosition.csv'
+    'ZoneMeanAirTemperature.csv'
 
-    df = load_csv('ZoneElectricEquipmentElectricPower.csv')
-    print(f'Plotting {df.columns[6]}')
-    plot_annual(df.iloc[:,6].values, title='Electric Power', filename='elec-power.pdf')
+    df = load_csv('ZoneMechanicalVentilationMassFlowRate.csv')
+    print(f'Collecting {df.columns[18]}')
+    df_airtemp = df.copy()
 
     df = load_csv('building_data.csv')
-    print(f'Plotting {df.columns[19]}')
-    plot_annual(df.iloc[:,19].values, title='Total HVAC Electricity Usage', filename='hvac-power.pdf')
+    print(f'Plotting building data')
+    plot_annual(df.iloc[:,19].values, title='Total HVAC Electricity Usage', filename='total-hvac.pdf')
+    plot_annual(df.iloc[:,1].values, title=df.columns[1], filename='total-occ.pdf')
+    plot_annual(df.iloc[:,2].values, title=df.columns[2], filename='total-cool.pdf')
+    plot_annual(df.iloc[:,5].values, title=df.columns[5], filename='total-heat.pdf')
+    plot_annual(df.iloc[:,6].values, title=df.columns[6], filename='total-gas.pdf')
 
+    open_idx = np.where(df['Operating Time'] == 'Yes')[0]
+    plt.scatter(df_heatset.iloc[open_idx,18].values, df_airtemp.iloc[open_idx,18].values)
+    plt.show()
+    plt.close()
+    
     pdb.set_trace()
 
 
@@ -75,4 +118,4 @@ def plot_pngs():
         plt.close()
 
 if __name__ == '__main__':
-    main()
+    airflowplots()
